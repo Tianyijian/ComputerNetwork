63445bc84078bf326b03ddf94379c584a6356094
Tianyijian <2624132357@qq.com>
2018-10-27 16:16:39 +0800

    基本代理功能，过滤，屏蔽，钓鱼

---
+++ src/main/MyProxy.java
@@ -0,0 +1,47 @@
+package main;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MyProxy {
+
+    private int port = 8888;
+    private ServerSocket serverSocket;
+    private ExecutorService executorService;    //线程池
+    private final int POOL_SIZE = 4;            //单个CPU时线程池中工作线程的数目
+    private int count = 0;        //线程计数
+    
+    public MyProxy() throws IOException {
+        serverSocket = new ServerSocket(port);
+        //根据CPU数目创建线程池
+        executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * POOL_SIZE);
+        System.out.println("Start server...");
+    }
+
+
+    public void service() {
+        while (true) {
+            Socket socket = null;
+            try {
+                socket = serverSocket.accept();
+                executorService.execute(new ClientHandle(socket, count++));  
+//                new SingleClient(socket).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                
+            }
+        }
+
+    }
+    
+    
+    public static void main(String[] args) throws IOException {
+        new MyProxy().service();
+    }
+    
+
+}
---
+++ src/main/MyServer.java
@@ -0,0 +1,70 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MyServer {
+
+    private int port = 8888;
+    private ServerSocket serverSocket;
+
+    public MyServer() throws IOException {
+        serverSocket = new ServerSocket(port);
+        System.out.println("Start server...");
+    }
+
+    // 向socket输出流写数据，向对方发数据
+    private PrintWriter getWriter(Socket socket) throws IOException {
+        OutputStream outputStream = socket.getOutputStream();
+        return new PrintWriter(outputStream, true);
+    }
+
+    // 从socket输入流读数据，接收对方的数据
+    private BufferedReader getReader(Socket socket) throws IOException {
+        InputStream inputStream = socket.getInputStream();
+        return new BufferedReader(new InputStreamReader(inputStream));
+    }
+
+    public void service() {
+        while (true) {
+            Socket socket = null;
+            try {
+                socket = serverSocket.accept();
+                System.out.println("New connection accepted: " + socket.getInetAddress() + ":" + socket.getPort());
+                BufferedReader br = getReader(socket);
+                PrintWriter pw = getWriter(socket);
+                String msg = null;
+                while ((msg = br.readLine()) != null) {
+                    System.out.println("Receive data: " + msg);
+                    pw.println("echo: " + msg); // 返回数据
+                    if (msg.equals("bye")) {
+                        break;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                // 关闭套接字
+                try {
+                    if (socket != null) {
+                        socket.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        new MyServer().service();
+    }
+}
